3fe20059d6f34f4b63eeb0f6c3cd61e28698ade7
umasree davuluri <umasreedavuluri9@gmail.com>
2023-06-27 21:49:01 -0500

    First commit

---
+++ main.py
@@ -0,0 +1,6 @@
+## to do
+print("Enter a todo:")
+##string1 = input("Enter a todo:")
+#str1 = " Please"
+str1 = int(input("Please give an input:\t\t"))
+print (str1)
---
+++ new_list.py
@@ -0,0 +1,18 @@
+## to do list
+##string1 = input("Enter a todo:")
+# str1 = " Please"
+var1 = int(input('Test input1:'))
+var2 = int(input('Test input2:'))
+var3 = int(input('Test input3:'))
+##print (str1)
+intlist = [var1, var2, var3]
+## to print input type
+##print(type(str1))
+var4 = type(var1)  ## for testing type
+# var4 = var1 ## var4 will be int only when value is assigned
+var4 = 'String'  ## var4 will be int only when value is assigned
+# var2 = str1
+# var2 = var2 * str1
+print("Type of VAR4", type(var4))
+print(var4)
+print(type(intlist))
